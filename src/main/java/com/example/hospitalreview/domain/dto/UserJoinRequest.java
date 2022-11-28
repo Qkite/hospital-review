2227d081f5547360c6434b4ab1cdffee9c7e4d82
Qkite <yeonjigu4@gmail.com>
2022-11-28 15:47:43 +0900

    testcode 작성하기

--- src/main/java/com/example/hospitalreview/domain/dto/UserJoinRequest.java
+++ src/main/java/com/example/hospitalreview/domain/dto/UserJoinRequest.java
@@ -3,9 +3,11 @@ package com.example.hospitalreview.domain.dto;
 
 import com.example.hospitalreview.domain.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,6 +16,8 @@ public class UserJoinRequest {
     private String userName;
     private String password;
     private String email;
+
+
 
     public User toEntity(){
         return User.builder()
